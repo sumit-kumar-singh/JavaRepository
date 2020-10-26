e03105f8c8ec608f68282810a5ef9f5096669319
Ritesh Kumar <riteshnagwan@gmail.com>
2020-10-26 18:23:05 +0530

    first commit

---
+++ demo/Test.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Test {
+
+    public static void main(String[] args){
+
+        System.out.println("hello world");
+    
+    	System.out.println("using git");
+    }
+}
